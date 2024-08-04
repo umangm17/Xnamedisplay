418a573bbfee54dca94008487fbb87b5e2a51a39
umang Malviya <umangmalviya51@gmail.com>
2024-08-04 20:39:13 +0530

    my last submit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,50 +1,3 @@
-// import { useState } from "react";
-// import React from "react";
-// import styles from "./Form.module.css";
-// function Myform() {
-//   const [data, setData] = useState({ firstname: "", lastname: "" });
-//   const [fullName, setFullName] = useState("");
-//   function handleChange(e) {
-//     const { name, value } = e.target;
-//     setData((prevFormData) => ({ ...prevFormData, [name]: value }));
-//   }
-//   function handleSubmit(e) {
-//     e.preventDefault();
-
-//     setFullName(`${data.firstname} ${data.lastname}`);
-//   }
-
-//   return (
-//     <>
-//       <div className={styles.display}>Full Named Display</div>
-//       <form onSubmit={handleSubmit}>
-//         <label>FirstName :</label>
-//         <input
-//           type="text"
-//           id="name"
-//           name="firstname"
-//           value={data.name}
-//           onChange={handleChange}
-//           required
-//         ></input>
-//         <div>
-//           <label>LastName :</label>
-//           <input
-//             type="text"
-//             name="lastname"
-//             value={data.name}
-//             onChange={handleChange}
-//             required
-//           ></input>
-//         </div>
-//         <button type="submit">Submit</button>
-//         <div>{fullName && <div>Full Name: {fullName}</div>}</div>
-//       </form>
-//     </>
-//   );
-// }
-
-// export default Myform;
 import React, { useState } from "react";
 import styles from "./Form.module.css";
 
@@ -61,9 +14,6 @@ function Myform() {
     e.preventDefault();
     setFullName(`${data.firstname} ${data.lastname}`);
   }
-
-  const isFormValid =
-    data.firstname.trim() !== "" && data.lastname.trim() !== "";
 
   return (
     <>
